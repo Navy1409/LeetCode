584c469352657ab5b8734537af9536fa4d7679d2
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-02-12 14:48:00 +0530

    Time: 4 ms (93.39%), Space: 18.4 MB (29.62%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        int i;
+        vector<int> ans;
+        for(auto i: nums){
+            m[i]++;
+        }
+        for(auto i: m){
+            if(i.second>(nums.size()/3)){
+                ans.push_back(i.first);
+            }
+        }
+        return ans;
+    }
+};
