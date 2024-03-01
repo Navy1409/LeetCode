68c78765adcd9934716d2aa106c256dd32352a9d
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-01 16:59:25 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (12.99%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int count=0;
-        for(int i=0; i<s.size(); i++){
+        int count=0, len=s.size();
+        for(int i=0; i<len; i++){
           if(s[i]=='1'){
               count++;
           }
