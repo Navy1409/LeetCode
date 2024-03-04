bdca1d01c4ec1e9d4bfe73c69e2396e4b2ecff2b
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-04 16:49:07 +0530

    Time: 3 ms (86.63%), Space: 16.9 MB (23.53%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -5,59 +5,22 @@ public:
         int N = inputArray.size();
         int M = 0;
         for (int i = 0; i < N; i++)
-
-        M = max(M, inputArray[i]);
- 
-
-    // Initializing countArray[] with 0
-
-    vector<int> countArray(M + 1, 0);
- 
-
-    // Mapping each element of inputArray[] as an index
-
-    // of countArray[] array
- 
-
-    for (int i = 0; i < N; i++)
-
-        countArray[inputArray[i]]++;
- 
-
-    // Calculating prefix sum at every index
-
-    // of array countArray[]
-
-    for (int i = 1; i <= M; i++)
-
-        countArray[i] += countArray[i - 1];
- 
-
-    // Creating outputArray[] from countArray[] array
-
-    vector<int> outputArray(N);
- 
-
-    for (int i = N - 1; i >= 0; i--)
- 
-
-    {
-
-        outputArray[countArray[inputArray[i]] - 1]
-
-            = inputArray[i];
- 
-
-        countArray[inputArray[i]]--;
-
-    }
-         for (int i =0; i < N; i++)
- 
-
-    {
-             inputArray[i]=outputArray[i];
-
-    }
+            M = max(M, inputArray[i]);
+        vector<int> countArray(M + 1, 0);
+        for (int i = 0; i < N; i++)
+            countArray[inputArray[i]]++;
+        for (int i = 1; i <= M; i++)
+            countArray[i] += countArray[i - 1];
+        vector<int> outputArray(N);
+        for (int i = N - 1; i >= 0; i--)
+        {
+            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+            countArray[inputArray[i]]--;
+        }
+        for (int i =0; i < N; i++)
+        {
+            inputArray[i]=outputArray[i];
+        }
     }
     int bagOfTokensScore(vector<int>& t, int p) {
         int s=0, len= t.size();
