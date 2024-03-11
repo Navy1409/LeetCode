7379b93b364efece60dbcaabbf8ecce743b9e0f4
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-11 18:40:47 +0530

    Time: 44 ms (25.13%), Space: 12.9 MB (29.53%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,41 +1,23 @@
-// class Solution {
-// public:
-//     string removeDuplicates(string s) {
-//         int len=s.size();
-//         stack<char> ans;
-//         for(int i=0; i<len; i++){
-//             if(ans.empty()){
-//                 ans.push(s[i]);
-//             }
-//             else if(s[i]!=ans.top()){
-//                 ans.push(s[i]);
-//             }
-//             else{
-//                 ans.pop();
-//             }
-//         }
-//         string a="";
-//         while(!ans.empty()){
-//             cout<<ans.top();
-//             a+=ans.top();
-//             ans.pop();
-//         }
-//         reverse(a.begin(),a.end());
-//         return a;
-//     }
-// };
-
 class Solution {
 public:
     string removeDuplicates(string s) {
-        int len = s.size();
-        string ans = "";
-        for (int i = 0; i < len; i++) {
-            if (!ans.empty() && ans.back() == s[i]) {
-                continue;
+        int len=s.size();
+        stack<char> ans;
+        for(int i=0; i<len; i++){
+            if(ans.empty()||s[i]!=ans.top()){
+                ans.push(s[i]);
             }
-            ans.push_back(s[i]);
+            else{
+                ans.pop();
+            }
         }
-        return ans;
+        string a="";
+        while(!ans.empty()){
+            cout<<ans.top();
+            a+=ans.top();
+            ans.pop();
+        }
+        reverse(a.begin(),a.end());
+        return a;
     }
 };
