1117318b08726b2691c5b1f818e3c6d8f5f5faba
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-11 18:33:18 +0530

    Time: 126 ms (20.12%), Space: 12.8 MB (29.53%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        int len=s.size();
+        stack<char> ans;
+        for(int i=0; i<len; i++){
+            if(ans.empty()||s[i]!=ans.top()){
+                cout<<s[i]<<endl;
+                ans.push(s[i]);
+            }
+            else{
+                ans.pop();
+            }
+        }
+        string a="";
+        while(!ans.empty()){
+            cout<<ans.top();
+            a+=ans.top();
+            ans.pop();
+        }
+        reverse(a.begin(),a.end());
+        return a;
+    }
+};
