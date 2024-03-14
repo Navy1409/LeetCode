cb102b8a1a8a4d4b974e930abf42c4fda5d55271
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-14 18:30:36 +0530

    Time: 2 ms (51.38%), Space: 7.2 MB (32.11%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int m=a, n=b, c=0;
+        if(b>a){
+            m=b;
+            n=a;
+        }
+        for(int i=1; i<=m; i++){
+            if(m%i==0&&n%i==0){
+                c++;
+            }
+        }
+        return c;
+    }
+};
