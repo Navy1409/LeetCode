a8705edd057fbc6c79341bdecf2930e0e0c7c877
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-14 14:25:06 +0530

    Time: 10 ms (26.45%), Space: 13.1 MB (48%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int o) {
-        std::sort(nums.begin(), nums.end());
-        while (std::binary_search(nums.begin(), nums.end(), o)) {
+        sort(nums.begin(), nums.end());
+        while (binary_search(nums.begin(), nums.end(), o)) {
             o *= 2;
         }
         return o;
