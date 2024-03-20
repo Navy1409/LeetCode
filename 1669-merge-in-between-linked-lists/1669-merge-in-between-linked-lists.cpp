633ae9cfd171b375a5b8f1a6f51b9529022420b1
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-03-20 17:34:04 +0530

    Time: 165 ms (91.35%), Space: 98 MB (82.89%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -1,33 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        ListNode* tail2;
-        ListNode* temp2=list2;
-        while(temp2->next!=NULL){
-            temp2=temp2->next;
+        ListNode* temp1 = list1;
+        
+        // Move temp1 to the node at position (a - 1)
+        for (int i = 0; i < a - 1; i++) {
+            temp1 = temp1->next;
         }
-        tail2=temp2;
-        temp2=list1;
-        while(b--){
-            temp2=temp2->next;
+        
+        // Find the node at position (b + 1)
+        ListNode* temp2 = temp1;
+        for (int i = 0; i < b - a + 2; i++) {
+            temp2 = temp2->next;
         }
-        tail2->next=temp2->next;
-        ListNode* temp1=list1, *prev;
-        while(a--){
-            prev=temp1;
-            temp1=temp1->next;
+        
+        // Find the tail node of list2
+        ListNode* tail2 = list2;
+        while (tail2->next != nullptr) {
+            tail2 = tail2->next;
         }
-        prev->next=list2;
+        
+        // Connect the tail of list2 to temp2
+        tail2->next = temp2;
+        
+        // Connect the node at (a - 1) to list2
+        temp1->next = list2;
+        
         return list1;
     }
 };
