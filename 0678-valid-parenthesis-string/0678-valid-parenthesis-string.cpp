386bc390deae7691b790acb64e7ea9bd5100bbf0
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-07 23:00:40 +0530

    Time: 3 ms (45.5%), Space: 7.4 MB (53.16%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,27 +1,64 @@
+// class Solution {
+// public:
+//     bool checkValidString(string s) {
+//         int starCount=0, ob=0, cb=0;
+//         int len= s.size();
+//         for(int i=0; i<len; i++){
+//             if(s[i]=='('){
+//                 ob++;
+//             }
+//             else if(s[i]==')'){
+//                 cb++;
+//             }
+//             else if(s[i]=='*'){
+//                 starCount++;
+//             }
+//         }
+//         cout<<ob<<" "<<cb<<" "<<starCount;
+//         if(ob==cb){
+//             return true;
+//         }
+//         if(abs(ob-cb)<=starCount){
+//             return true;
+//         }
+//         return false;
+
+//     }
+// };
+
+#include <iostream>
+#include <string>
+#include <stack>
+
 class Solution {
 public:
-    bool checkValidString(string s) {
-        int starCount=0, ob=0, cb=0;
-        int len= s.size();
-        for(int i=0; i<len; i++){
-            if(s[i]=='('){
-                ob++;
-            }
-            else if(s[i]==')'){
-                cb++;
-            }
-            else if(s[i]=='*'){
-                starCount++;
+    bool checkValidString(std::string s) {
+        std::stack<int> openStack, starStack;
+        
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                openStack.push(i);
+            } else if (s[i] == '*') {
+                starStack.push(i);
+            } else { // s[i] == ')'
+                if (!openStack.empty()) {
+                    openStack.pop();
+                } else if (!starStack.empty()) {
+                    starStack.pop();
+                } else {
+                    return false; // No matching '(' or '*' found
+                }
             }
         }
-        cout<<ob<<" "<<cb<<" "<<starCount;
-        if(ob==cb){
-            return true;
+        
+        while (!openStack.empty() && !starStack.empty()) {
+            if (openStack.top() > starStack.top()) {
+                return false; // '*' appears after '('
+            }
+            openStack.pop();
+            starStack.pop();
         }
-        if(abs(ob-cb)<=starCount){
-            return true;
-        }
-        return false;
-
+        
+        return openStack.empty();
     }
 };
