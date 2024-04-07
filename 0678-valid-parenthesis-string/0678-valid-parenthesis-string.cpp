aa49da67e42852bb421919de8459aa30085dec8c
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-07 23:09:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,64 +1,69 @@
-// class Solution {
-// public:
-//     bool checkValidString(string s) {
-//         int starCount=0, ob=0, cb=0;
-//         int len= s.size();
-//         for(int i=0; i<len; i++){
-//             if(s[i]=='('){
-//                 ob++;
-//             }
-//             else if(s[i]==')'){
-//                 cb++;
-//             }
-//             else if(s[i]=='*'){
-//                 starCount++;
-//             }
-//         }
-//         cout<<ob<<" "<<cb<<" "<<starCount;
-//         if(ob==cb){
-//             return true;
-//         }
-//         if(abs(ob-cb)<=starCount){
-//             return true;
-//         }
-//         return false;
-
-//     }
-// };
-
 #include <iostream>
 #include <string>
-#include <stack>
 
 class Solution {
 public:
     bool checkValidString(std::string s) {
-        std::stack<int> openStack, starStack;
-        
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == '(') {
-                openStack.push(i);
-            } else if (s[i] == '*') {
-                starStack.push(i);
-            } else { // s[i] == ')'
-                if (!openStack.empty()) {
-                    openStack.pop();
-                } else if (!starStack.empty()) {
-                    starStack.pop();
+        int starCount = 0, ob = 0;
+
+        for (char ch : s) {
+            if (ch == '(') {
+                ob++;
+            } else if (ch == '*') {
+                starCount++;
+            } else {
+                if (ob != 0) {
+                    ob--;
+                } else if (starCount != 0) {
+                    starCount--;
                 } else {
-                    return false; // No matching '(' or '*' found
+                    return false;
                 }
             }
         }
-        
-        while (!openStack.empty() && !starStack.empty()) {
-            if (openStack.top() > starStack.top()) {
-                return false; // '*' appears after '('
-            }
-            openStack.pop();
-            starStack.pop();
+
+        if (ob > starCount) {
+            return false;
         }
-        
-        return openStack.empty();
+
+        return true;
     }
 };
+
+
+// #include <iostream>
+// #include <string>
+// #include <stack>
+
+// class Solution {
+// public:
+//     bool checkValidString(std::string s) {
+//         std::stack<int> openStack, starStack;
+        
+//         for (int i = 0; i < s.size(); ++i) {
+//             if (s[i] == '(') {
+//                 openStack.push(i);
+//             } else if (s[i] == '*') {
+//                 starStack.push(i);
+//             } else { // s[i] == ')'
+//                 if (!openStack.empty()) {
+//                     openStack.pop();
+//                 } else if (!starStack.empty()) {
+//                     starStack.pop();
+//                 } else {
+//                     return false; // No matching '(' or '*' found
+//                 }
+//             }
+//         }
+        
+//         while (!openStack.empty() && !starStack.empty()) {
+//             if (openStack.top() > starStack.top()) {
+//                 return false; // '*' appears after '('
+//             }
+//             openStack.pop();
+//             starStack.pop();
+//         }
+        
+//         return openStack.empty();
+//     }
+// };
