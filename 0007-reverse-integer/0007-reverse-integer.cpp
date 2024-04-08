3184ccbc392bae6d9bb2c5da21541dff3d4fdd0f
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-08 23:17:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    long long reverse(long long x) {
-        long long n=x;
-        vector<long long> nums;
-        while(n){
-            nums.push_back(n%10);
-            n=n/10;
+    int reverse(int x) {
+        int r=0;
+        while(x){
+            r=r*10+x%10;
+            x=x/10;
         }
-        long long c=nums.size()-1;
-        long long num=0;
-        for(long long i: nums){
-            num+=i*pow(10,c);
-            c--;
-        }
-        if(num>pow(2, 31)-1||num<pow(-2, 31)){
+        if(r>INT_MAX||r<INT_MIN){
             return 0;
         }
-        return num;
+        return r;
     }
 };
