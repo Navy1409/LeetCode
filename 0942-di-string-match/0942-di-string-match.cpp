6504613271ea56bc0cc0123203ea324135c39a75
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-10 14:30:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.cpp
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -2,30 +2,41 @@ class Solution {
 public:
     vector<int> diStringMatch(string s) {
         int len= s.size();
-        vector<int> q,a;
-        for(int i=0; i<=len ;i++){
-            q.push_back(i);
-        }
+        // vector<int> q,a;
+        // for(int i=0; i<=len ;i++){
+        //     q.push_back(i);
+        // }
+        // int f=0, r=len;
+        // for(int i=0; i<len-1; i++){
+        //     if((s[i]=='I'&& s[i]=='I')||(s[i]=='I'&&s[i]=='D')){
+        //         a.push_back(q[f]);
+        //         f++;
+        //     }
+        //     else{
+        //         a.push_back(q[r]);
+        //         r--;
+        //     }
+        // }
+        // if(s[len-1]=='I'){
+        //     a.push_back(q[f]);
+        //     f++;
+        // }
+        // else{
+        //     a.push_back(q[r]);
+        //     r--;
+        // }
+        // a.push_back(q[f]);
+        vector<int> a(len+1);
         int f=0, r=len;
-        for(int i=0; i<len-1; i++){
-            if((s[i]=='I'&& s[i]=='I')||(s[i]=='I'&&s[i]=='D')){
-                a.push_back(q[f]);
-                f++;
+        for(int i=0; i<len; i++){
+            if(s[i]=='I'){
+                a[i]=f++;
             }
             else{
-                a.push_back(q[r]);
-                r--;
+                a[i]=r--;
             }
         }
-        if(s[len-1]=='I'){
-            a.push_back(q[f]);
-            f++;
-        }
-        else{
-            a.push_back(q[r]);
-            r--;
-        }
-        a.push_back(q[f]);
+        // a.push_back(f);
         return a;
     }
 };
