f828b8d8de37f7e90c964fb91c1a812e8491c62d
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-10 13:55:02 +0530

    Time: 18 ms (18.53%), Space: 10.8 MB (91.52%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& q) {
+        sort(q.begin(), q.end());
+        int n=q.size(), j=0;
+        vector<int> v;
+        for(int i=0; i<n; i++){
+            if(i%2==0){
+                v.push_back(q[j]);
+                j++;
+            }
+            else{
+                v.push_back(0);
+            }
+        }
+        int flag;
+        if(n%2==0){
+            flag=1;
+        }
+        else{
+            flag=0;
+        }
+        int i=0;
+        while(find(v.begin(), v.end(), 0)!=v.end()){
+            if(v[i]==0 && flag==1){
+                v[i]=q[j];
+                j++;
+                flag=0;
+            }
+            else if(v[i]==0 && flag==0){
+                flag=1;
+            }
+            i++;
+            if(i==n){
+                i=0;
+            }
+        }
+        return v;
+        
+    }
+};
