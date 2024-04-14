da155b3c2b0d8b47fcb146c736cc0f9ebcf2e32f
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-14 21:21:49 +0530

    Time: 0 ms (100%), Space: 14.7 MB (48.75%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -9,15 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        if (!root) return  0;
-        int sum=0;
-        if (root->left && !root->left->left && !root->left->right)  
-            sum+=root->left->val;
-        sum+=sumOfLeftLeaves(root->left)+sumOfLeftLeaves(root->right);
+        if (!root) return 0;
+
+        int sum = 0;
+        if (root->left && !root->left->left && !root->left->right) {
+            sum = root->left->val;
+        }
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
         return sum;
     }
 };
