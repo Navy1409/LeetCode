3d5d9071feebd08a70b4fd88a2b2e90512b883c9
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-16 14:53:36 +0530

    Time: 36 ms (38.71%), Space: 21.9 MB (12.28%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,31 +1,65 @@
 class Solution {
+    private:
+    vector<int> nextSmaller(vector<int> a, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+        for (int i = n - 1; i >= 0; i--) {
+            int curr = a[i];
+            while (s.top() != -1 && a[s.top()] >= curr) {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    vector<int> prevSmaller(vector<int> a, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            int curr = a[i];
+            while (s.top() != -1 && a[s.top()] >= curr) {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> nextS;
+        nextS = nextSmaller(heights, n);
+        vector<int> prevS;
+        prevS = prevSmaller(heights, n);
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            if (nextS[i] == -1) {
+                nextS[i] = n;
+            }
+            ans = max((heights[i] * (nextS[i] - prevS[i] - 1)), ans);
+        }
+        return ans;
+    }
+
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
-            return 0;
-        
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<int> heights(cols + 1, 0); // Include an extra element for easier calculation
-        int maxArea = 0;
-        
-        for (const auto& row : matrix) {
-            for (int i = 0; i < cols; i++) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
-            }
-            
-            // Calculate max area using histogram method
-            int n = heights.size(); // Number of bars in the histogram
-            
-            for (int i = 0; i < n; i++) {
-                for (int j = i, minHeight = INT_MAX; j < n; j++) {
-                    minHeight = min(minHeight, heights[j]);
-                    int area = minHeight * (j - i + 1);
-                    maxArea = max(maxArea, area);
+        int n=matrix[0].size();
+        vector<int> heights(n,0);
+        int ans = -1;
+        for (int i = 0; i < matrix.size(); i++) {
+            for(int j=0; j<n;j++ ){
+                if(matrix[i][j]=='1'){
+                    heights[j]++;
+                }
+                else{
+                    heights[j]=0;
                 }
             }
+            ans=max(ans, largestRectangleArea(heights));
         }
-        
-        return maxArea;
+        return ans;
     }
 };
