54b540b77feb6c342af13bf28d78408991cd7458
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-19 09:59:34 +0530

    Time: 4 ms (17.92%), Space: 7.6 MB (38.33%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,22 +1,46 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n=needle.size(), h=haystack.size();
+//         int ans=-1;
+//         for(int i=0; i<h;i++){
+//             for(int j=0; j<n; j++){
+//                 if(needle[j]!=haystack[i+j]){
+//                     break;
+//                 }
+//                 else if( j==n-1){
+//                     ans=i;
+//                     break;
+//                 }
+//             }
+//             if(ans!=-1){
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=needle.size(), h=haystack.size();
-        int ans=-1;
-        for(int i=0; i<h;i++){
-            for(int j=0; j<n; j++){
-                if(needle[j]!=haystack[i+j]){
-                    break;
-                }
-                else if( j==n-1){
-                    ans=i;
-                    break;
-                }
+        int n = needle.size();
+        int h = haystack.size();
+        
+        if (n == 0) {
+            return 0; // Empty needle, so it's always found at position 0
+        }
+        
+        for (int i = 0; i <= h - n; ++i) {
+            int j = 0;
+            while (j < n && haystack[i + j] == needle[j]) {
+                j++;
             }
-            if(ans!=-1){
-                break;
+            if (j == n) {
+                return i; // Match found at position i
             }
         }
-        return ans;
+        
+        return -1; // Match not found
     }
 };
