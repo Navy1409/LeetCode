e6f4bb515db97e2aead609c38aa1bf5eb7595415
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-19 10:01:09 +0530

    Time: 3 ms (42.19%), Space: 7.5 MB (79.62%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,3 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
+
 // class Solution {
 // public:
 //     int strStr(string haystack, string needle) {
@@ -21,26 +28,28 @@
 //     }
 // };
 
-class Solution {
-public:
-    int strStr(string haystack, string needle) {
-        int n = needle.size();
-        int h = haystack.size();
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int n = needle.size();
+//         int h = haystack.size();
         
-        if (n == 0) {
-            return 0; // Empty needle, so it's always found at position 0
-        }
+//         if (n == 0) {
+//             return 0; // Empty needle, so it's always found at position 0
+//         }
         
-        for (int i = 0; i <= h - n; ++i) {
-            int j = 0;
-            while (j < n && haystack[i + j] == needle[j]) {
-                j++;
-            }
-            if (j == n) {
-                return i; // Match found at position i
-            }
-        }
+//         for (int i = 0; i <= h - n; ++i) {
+//             int j = 0;
+//             while (j < n && haystack[i + j] == needle[j]) {
+//                 j++;
+//             }
+//             if (j == n) {
+//                 return i; // Match found at position i
+//             }
+//         }
         
-        return -1; // Match not found
-    }
-};
+//         return -1; // Match not found
+//     }
+// };
+
+
