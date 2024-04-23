3bb78a924bb8db9286a71e8d40333e4ecbb46136
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-23 10:21:53 +0530

    Time: 3 ms (70.45%), Space: 12.1 MB (7.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,28 +1,50 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         if(find(nums.begin(), nums.end(), target)==nums.end()){
+//             int ans=0;
+//         int len =nums.size();
+//         for(int i=0; i<len; i++){
+//             if(nums[i]<target && i==len-1){
+//                 return len;
+//             }
+//             if(nums[i]>target){
+//                 ans=i;
+//                 break;
+//             }
+//         }
+//         return ans;
+//         }
+//         int ans=0;
+//         int len =nums.size();
+//         for(int i=0; i<len; i++){
+//             if(nums[i]==target){
+//                 ans=i;
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if(find(nums.begin(), nums.end(), target)==nums.end()){
-            int ans=0;
-        int len =nums.size();
-        for(int i=0; i<len; i++){
-            if(nums[i]<target && i==len-1){
-                return len;
-            }
-            if(nums[i]>target){
-                ans=i;
-                break;
-            }
-        }
-        return ans;
-        }
-        int ans=0;
-        int len =nums.size();
-        for(int i=0; i<len; i++){
-            if(nums[i]==target){
-                ans=i;
-                break;
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return ans;
+        
+        return left; // Insert position
     }
 };
