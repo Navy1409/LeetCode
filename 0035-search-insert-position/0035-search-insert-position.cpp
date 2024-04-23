737da591dc280d5cca004e466aeb282426a3c8cc
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-23 10:24:30 +0530

    Time: 7 ms (21.49%), Space: 12.2 MB (7.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,48 +1,48 @@
-// class Solution {
-// public:
-//     int searchInsert(vector<int>& nums, int target) {
-//         if(find(nums.begin(), nums.end(), target)==nums.end()){
-//             int ans=0;
-//         int len =nums.size();
-//         for(int i=0; i<len; i++){
-//             if(nums[i]<target && i==len-1){
-//                 return len;
-//             }
-//             if(nums[i]>target){
-//                 ans=i;
-//                 break;
-//             }
-//         }
-//         return ans;
-//         }
-//         int ans=0;
-//         int len =nums.size();
-//         for(int i=0; i<len; i++){
-//             if(nums[i]==target){
-//                 ans=i;
-//                 break;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int start=0, end= nums.size()-1;
-        while(start<=end)
-        {
-            int mid = (start+end)/2;
-            if(nums[mid]==target)
-                return mid;
-            else if(target > nums[mid])
-            {
-                start = mid+1;
+        if(find(nums.begin(), nums.end(), target)==nums.end()){
+            int ans=0;
+        int len =nums.size();
+        for(int i=0; i<len; i++){
+            if(nums[i]<target && i==len-1){
+                return len;
             }
-            else
-                end = mid-1;
+            if(nums[i]>target){
+                ans=i;
+                break;
+            }
         }
-        return start;
+        return ans;
+        }
+        int ans=0;
+        int len =nums.size();
+        for(int i=0; i<len; i++){
+            if(nums[i]==target){
+                ans=i;
+                break;
+            }
+        }
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int start=0, end= nums.size()-1;
+//         while(start<=end)
+//         {
+//             int mid = (start+end)/2;
+//             if(nums[mid]==target)
+//                 return mid;
+//             else if(target > nums[mid])
+//             {
+//                 start = mid+1;
+//             }
+//             else
+//                 end = mid-1;
+//         }
+//         return start;
+//     }
+// };
