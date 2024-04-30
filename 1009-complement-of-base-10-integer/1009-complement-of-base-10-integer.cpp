c43aee6ed84eea3441bbbdb878714a1f92cdcf41
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-30 19:17:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0){
+            return 0;
+        }
+        vector<int> a;
+        while(n){
+            a.insert(a.begin(), n%2);
+            n/=2;
+        }
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==1){
+                a[i]=0;
+            }
+            else{
+                a[i]=1;
+            }
+        }
+        int j=0;
+        int ans=0;
+        for(int i=a.size()-1; i>=0; i--){
+            ans+=a[i]*pow(2,j);
+            j++;
+        }
+        return ans;
+    }
+};
