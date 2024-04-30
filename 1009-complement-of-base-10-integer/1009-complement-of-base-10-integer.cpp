d97fa48a60a8ea5bb02d283e066fbbdb56316ea7
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-04-30 19:26:31 +0530

    Time: 3 ms (28.62%), Space: 7.2 MB (7.35%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -4,25 +4,33 @@ public:
         if(n==0){
             return 1;
         }
-        vector<int> a;
-        while(n){
-            a.insert(a.begin(), n%2);
-            n/=2;
+        // vector<int> a;
+        // while(n){
+        //     a.insert(a.begin(), n%2);
+        //     n/=2;
+        // }
+        // for(int i=0; i<a.size(); i++){
+        //     if(a[i]==1){
+        //         a[i]=0;
+        //     }
+        //     else{
+        //         a[i]=1;
+        //     }
+        // }
+        // int j=0;
+        // int ans=0;
+        // for(int i=a.size()-1; i>=0; i--){
+        //     ans+=a[i]*pow(2,j);
+        //     j++;
+        // }
+        // return ans;
+        int m=n;
+        int mask=0;
+        while(m){
+            mask=(mask<<1)|1;
+            m=m>>1;
         }
-        for(int i=0; i<a.size(); i++){
-            if(a[i]==1){
-                a[i]=0;
-            }
-            else{
-                a[i]=1;
-            }
-        }
-        int j=0;
-        int ans=0;
-        for(int i=a.size()-1; i>=0; i--){
-            ans+=a[i]*pow(2,j);
-            j++;
-        }
+        int ans=(~n)&mask;
         return ans;
     }
 };
