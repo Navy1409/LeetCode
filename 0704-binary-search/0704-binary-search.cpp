c40ee3f7924e4cd2ab372449852483ca4c11c19c
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-05-03 22:33:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int f=0, l=nums.size()-1;
+        int m;
+        while(f<=l){
+            m=(l+f)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<target){
+                f=m+1;
+            }
+            else{
+                l=m-11;
+            }
+        }
+        return -1;
+    }
+};
