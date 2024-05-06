203c0344653a02e07e35189c24d13eb5e81ac948
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-05-06 14:22:58 +0530

    Time: 179 ms (16.06%), Space: 97.4 MB (67.89%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        int len= nums.size();
+        int len = nums.size();
         map<int, int> mp;
-        for(auto i: nums){
+        for (auto i : nums) {
             mp[i]++;
         }
         int c, el;
-        for(auto i: mp){
-            if(i.second*2> len){
-               c=i.second;
-               el=i.first;
-                int ci=0;
-        for(int i=0; i<len; i++){
-            if(nums[i]==el){
-                ci++;
-                if(ci*2>i+1 && (c-ci)*2>(len-i-1)){
-                    return i;
+        for (auto i : mp) {
+            if (i.second * 2 > len) {
+                c = i.second;
+                el = i.first;
+                int ci = 0;
+                for (int i = 0; i < len; i++) {
+                    if (nums[i] == el) {
+                        ci++;
+                        if (ci * 2 > i + 1 && (c - ci) * 2 > (len - i - 1)) {
+                            return i;
+                        }
+                    }
                 }
+                break;
             }
         }
-               break;
-            }
-        }
-       
+
         return -1;
     }
 };
