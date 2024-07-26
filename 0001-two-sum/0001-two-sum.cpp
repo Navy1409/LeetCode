e717c97de1947058e51efc35205ff924b34aeb53
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-07-26 09:31:21 +0530

    Time: 7 ms (84.9%), Space: 16 MB (5.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> a;
+//         for(int i=0; i<nums.size();i++){
+//             for(int j=i+1; j<nums.size();j++){
+                
+//                 if((nums[i]+nums[j]==target)){
+//                     a.push_back(i);
+//                     a.push_back(j);
+//                     break;
+//                 }
+//             }
+//         }
+//         return a;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> a;
-        for(int i=0; i<nums.size();i++){
-            for(int j=i+1; j<nums.size();j++){
-                
-                if((nums[i]+nums[j]==target)){
-                    a.push_back(i);
-                    a.push_back(j);
-                    break;
-                }
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            hash[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end() && hash[complement] != i) {
+                return {i, hash[complement]};
             }
         }
-        return a;
+        return {};
     }
 };
