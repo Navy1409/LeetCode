35001011f3d52044cecb86b6eac61114bea9a343
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-08-03 13:02:18 +0530

    Time: 94 ms (5.29%), Space: 62.3 MB (19.78%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -2,16 +2,35 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int s=0, e=arr.size()-1;
-        int m;
-        while(s < e){
-            m = s + (e - s) / 2;
-            if(arr[m] < arr[m+1]){
-                s = m + 1;
+        int mid=s+(e-s)/2;
+        while(s<e){
+            mid=s+(e-s)/2;
+            if(arr[mid]<arr[mid+1]){
+                s=mid+1;
             }
             else{
-                e = m;
+                e=mid;
             }
         }
         return s;
     }
 };
+
+
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int s=0, e=arr.size()-1;
+//         int m;
+//         while(s < e){
+//             m = s + (e - s) / 2;
+//             if(arr[m] < arr[m+1]){
+//                 s = m + 1;
+//             }
+//             else{
+//                 e = m;
+//             }
+//         }
+//         return s;
+//     }
+// };
