91cfad2811ec2293d01530f45e23a43ab40a262e
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-03 15:59:29 +0530

    Time: 0 ms (100%), Space: 10.5 MB (56.01%) - LeetHub

---
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,61 @@
+class Solution {
+  public:
+  bool check(vector<int>& nums) {
+    //   int flag =0, index;
+    //   int ans=0;
+    //   vector<int> arr;
+    //   if(nums.size()>2){
+    //       for(int i=1; i<nums.size()-1; i++){
+    //           if(nums[i+1]>=nums[i]&&nums[i]<nums[i-1]){
+    //               index=i;
+    //               break;
+    //             }
+    //             else if(nums[i+1]<nums[i]&&(i+1==nums.size()-1)){
+    //                 index=nums.size()-1;
+    //             }
+    //             else{
+    //                 index=0;
+    //             }
+    //         }
+    //         for(int i= index; i<nums.size(); i++){
+    //             arr.push_back(nums[i]);
+    //         }
+    //         for(int i= 0; i<index; i++){
+    //             arr.push_back(nums[i]);
+    //         }
+    //         for(int i=0; i<arr.size()-1; i++){
+    //             if(arr[i+1]<arr[i]){
+    //                 flag++;
+    //             }
+    //         }
+    //         if(flag==0){
+    //             ans=1;
+    //         }
+    //         else{
+    //             ans=0;
+    //         }
+    //     }
+    //     else{
+    //        ans=1;
+    //     }
+    //     if(ans==0){
+    //         return false;
+    //     }
+    //     else{
+    //         return true;
+    //     }
+    int c=0;
+    for(int i=0; i<nums.size()-1; i++){
+        if(nums[i]>nums[i+1]){
+            c++;
+        }
+    }
+    if(nums[nums.size()-1]>nums[0]){
+        c++;
+    }
+    if(c>1){
+        return false;
+    }
+    return true;
+  }
+};
