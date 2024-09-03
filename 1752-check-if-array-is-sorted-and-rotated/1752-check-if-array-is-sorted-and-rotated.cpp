101c52719bfc595afe56e3d1cb4d68c9973cdea8
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-03 15:59:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,48 +1,69 @@
 class Solution {
   public:
   bool check(vector<int>& nums) {
-      int flag =0, index;
-      int ans=0;
-      vector<int> arr;
-      if(nums.size()>2){
-          for(int i=1; i<nums.size()-1; i++){
-              if(nums[i+1]>=nums[i]&&nums[i]<nums[i-1]){
-                  index=i;
-                  break;
-                }
-                else if(nums[i+1]<nums[i]&&(i+1==nums.size()-1)){
-                    index=nums.size()-1;
-                }
-                else{
-                    index=0;
-                }
-            }
-            for(int i= index; i<nums.size(); i++){
-                arr.push_back(nums[i]);
-            }
-            for(int i= 0; i<index; i++){
-                arr.push_back(nums[i]);
-            }
-            for(int i=0; i<arr.size()-1; i++){
-                if(arr[i+1]<arr[i]){
-                    flag++;
-                }
-            }
-            if(flag==0){
-                ans=1;
-            }
-            else{
-                ans=0;
-            }
+    //   int flag =0, index;
+    //   int ans=0;
+    //   vector<int> arr;
+    //   if(nums.size()>2){
+    //       for(int i=1; i<nums.size()-1; i++){
+    //           if(nums[i+1]>=nums[i]&&nums[i]<nums[i-1]){
+    //               index=i;
+    //               break;
+    //             }
+    //             else if(nums[i+1]<nums[i]&&(i+1==nums.size()-1)){
+    //                 index=nums.size()-1;
+    //             }
+    //             else{
+    //                 index=0;
+    //             }
+    //         }
+    //         for(int i= index; i<nums.size(); i++){
+    //             arr.push_back(nums[i]);
+    //         }
+    //         for(int i= 0; i<index; i++){
+    //             arr.push_back(nums[i]);
+    //         }
+    //         for(int i=0; i<arr.size()-1; i++){
+    //             if(arr[i+1]<arr[i]){
+    //                 flag++;
+    //             }
+    //         }
+    //         if(flag==0){
+    //             ans=1;
+    //         }
+    //         else{
+    //             ans=0;
+    //         }
+    //     }
+    //     else{
+    //        ans=1;
+    //     }
+    //     if(ans==0){
+    //         return false;
+    //     }
+    //     else{
+    //         return true;
+    //     }
+    int smallest=INT_MAX, smallIdx=-1;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i]<=smallest){
+            smallest=nums[i];
+            smallIdx=i;
         }
-        else{
-           ans=1;
-        }
-        if(ans==0){
+    }
+    for(int i=smallIdx; i<nums.size()-1; i++){
+        if(nums[i]>nums[i+1]){
             return false;
         }
-        else{
-            return true;
+    }
+    if(smallIdx != 0 && nums[0]<nums[nums.size()-1]){
+        return false;
+    }
+    for(int i=0; i<smallIdx-1; i++){
+        if(nums[i]>nums[i+1]){
+            return false;
         }
+    }
+    return true;
     }
 };
