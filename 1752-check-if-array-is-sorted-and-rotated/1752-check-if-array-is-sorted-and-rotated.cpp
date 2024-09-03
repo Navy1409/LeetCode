3666af2286dbf450f0ca6d48b7a2d4efbd3a8fab
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-03 16:00:44 +0530

    Time: 5 ms (19.69%), Space: 10.5 MB (56.01%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -44,14 +44,11 @@ class Solution {
     //     else{
     //         return true;
     //     }
-    int c=0;
-    for(int i=0; i<nums.size()-1; i++){
-        if(nums[i]>nums[i+1]){
+    int c=0, n=nums.size();
+    for(int i=0; i<n; i++){
+        if(nums[i]>nums[(i+1)%n]){
             c++;
         }
-    }
-    if(nums[nums.size()-1]>nums[0]){
-        c++;
     }
     if(c>1){
         return false;
