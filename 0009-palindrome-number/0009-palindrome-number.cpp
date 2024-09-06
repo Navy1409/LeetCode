4ac9108cee91fbe688479d41531d03ed8f24812f
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-06 10:00:04 +0530

    Time: 7 ms (71.83%), Space: 10.5 MB (13.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -40,8 +40,8 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string y=to_string(x);
-        for(int i=1; i<=(y.length()/2); i++){
-            if(y[i-1]!=y[y.length()-i]){
+        for(int i=0; i<=(y.length()/2); i++){
+            if(y[i]!=y[y.length()-i-1]){
                return false;
             }
         }
