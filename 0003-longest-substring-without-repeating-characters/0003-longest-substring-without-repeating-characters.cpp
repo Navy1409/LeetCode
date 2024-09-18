206f2edc6fb4aa9671c1789718cc9f2c7426fc3d
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-18 14:28:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,43 @@
-#include <string>
-#include <unordered_map>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        if (n <= 1) return n;
-
-        int ans = 0;
-        unordered_map<char, int> seen;
-        int left = 0, right = 0;
-
-        while (right < n) {
-            if (seen.find(s[right]) != seen.end() && seen[s[right]] >= left) {
-                ans = max(ans, right - left);
-                left = seen[s[right]] + 1;
+        int maxC=0, len= s.size(), start=0, end=0;
+        vector<char> vchar;
+        for(int i=0; i<len; i++){
+            maxC=max(maxC,(end-start));
+            if(find(vchar.begin()+start, vchar.end(), s[i])==vchar.end()){
+                end++;
+                vchar.push_back(s[i]);
             }
-            seen[s[right]] = right;
-            right++;
+            else{
+                start=find(vchar.begin()+start, vchar.end(), s[i])-vchar.begin()+1;
+                end++;
+                 vchar.push_back(s[i]);
+            }
         }
-
-        return max(ans, right - left);
+        return maxC;
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.size();
+//         if (n <= 1) return n;
+
+//         int ans = 0;
+//         unordered_map<char, int> seen;
+//         int left = 0, right = 0;
+
+//         while (right < n) {
+//             if (seen.find(s[right]) != seen.end() && seen[s[right]] >= left) {
+//                 ans = max(ans, right - left);
+//                 left = seen[s[right]] + 1;
+//             }
+//             seen[s[right]] = right;
+//             right++;
+//         }
+
+//         return max(ans, right - left);
+//     }
+// };
