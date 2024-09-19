982044262a34b800994b0e792dc6af11140ed4a5
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-19 20:07:05 +0530

    Time: 161 ms (68.03%), Space: 116.9 MB (66.78%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,18 +10,69 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> vals;
-        ListNode* temp=head;
-        while(temp){
-            vals.push_back(temp->val);
+    ListNode* reverse(ListNode* &head){
+        ListNode*prev=NULL, *curr=head, *forw=NULL;
+        while(curr!=NULL){
+            forw=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forw;
+        }
+        return prev;
+    }
+    int listLen(ListNode* &head){
+        ListNode*temp=head;
+        int len=0;
+        while(temp!=NULL){
+            len++;
             temp=temp->next;
         }
-        int l=0, r= vals.size()-1;
-        while(l<r&&vals[l]==vals[r]){
-            l++;
-            r--;
+        return len;
+    }
+    bool isPalindrome(ListNode* head) {
+        int len=listLen(head);
+        ListNode* slow=head, *fast=head;
+        if(len%2==0){
+            while(fast->next->next!=NULL){
+                slow=slow->next;
+                fast=fast->next->next;
+            }
         }
-        return l>=r;
+        else{
+            while(fast->next!=NULL){
+                slow=slow->next;
+                fast=fast->next->next;
+            }
+        }
+        ListNode* newHead= reverse(slow->next);
+        ListNode* newHeadTemp=newHead;
+        ListNode* headTemp=head;
+        bool ans=true;
+        while(newHeadTemp!=NULL){
+            if(newHeadTemp->val != headTemp->val){
+                ans=false;
+            }
+            newHeadTemp=newHeadTemp->next;
+            headTemp=headTemp->next;
+        }
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> vals;
+//         ListNode* temp=head;
+//         while(temp){
+//             vals.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         int l=0, r= vals.size()-1;
+//         while(l<r&&vals[l]==vals[r]){
+//             l++;
+//             r--;
+//         }
+//         return l>=r;
+//     }
+// };
