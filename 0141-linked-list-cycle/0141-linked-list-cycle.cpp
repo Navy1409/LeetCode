a374eb9cd11b45da18d72e98f3580d914c7619fc
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-19 15:23:16 +0530

    Time: 8 ms (71.1%), Space: 10.8 MB (86.28%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,30 +8,38 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *temp) {
-        vector<ListNode *>po;
-        if(temp==NULL){
-            return false;
-        }
-        while(temp->next!=NULL){
-            if(find(po.begin(),po.end(),temp)!=po.end()){
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
             }
-            po.push_back(temp);
-            temp=temp->next;
         }
         return false;
     }
 };
 
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     ListNode *next;
-//  *     ListNode(int x) : val(x), next(NULL) {}
-//  * };
-//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *temp) {
+//         vector<ListNode *>po;
+//         if(temp==NULL){
+//             return false;
+//         }
+//         while(temp->next!=NULL){
+//             if(find(po.begin(),po.end(),temp)!=po.end()){
+//                 return true;
+//             }
+//             po.push_back(temp);
+//             temp=temp->next;
+//         }
+//         return false;
+//     }
+// };
+
 // class Solution {
 // public:
 //     bool hasCycle(ListNode *head) {
