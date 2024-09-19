1938525a6def784211cfa6ef191e2fb63ebf0ea2
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-19 19:30:10 +0530

    Time: 3 ms (97.86%), Space: 10.2 MB (90.43%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,18 +8,42 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *temp) {
-        vector<ListNode *>po;
-        if(temp==NULL){
-            return NULL;
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL ){
+            return head;
         }
-        while(temp->next!=NULL){
-            if(find(po.begin(),po.end(),temp)!=po.end()){
-                return temp;
+        ListNode* slow=head;
+        ListNode*fast=head;
+        while(fast!=nullptr && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(fast!=slow){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow;
             }
-            po.push_back(temp);
-            temp=temp->next;
         }
-        return NULL;
+        return nullptr;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *temp) {
+//         vector<ListNode *>po;
+//         if(temp==NULL){
+//             return NULL;
+//         }
+//         while(temp->next!=NULL){
+//             if(find(po.begin(),po.end(),temp)!=po.end()){
+//                 return temp;
+//             }
+//             po.push_back(temp);
+//             temp=temp->next;
+//         }
+//         return NULL;
+//     }
+// };
