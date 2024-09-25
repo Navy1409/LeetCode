00c6fbe9aa370e794e4ffccc38b2d9366bf0a2eb
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-09-25 14:00:01 +0530

    Time: 0 ms (100%), Space: 14.7 MB (77.62%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,24 +11,52 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-         ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* first = dummy;
-        ListNode* second = dummy;
-
-        for (int i = 0; i <= n; ++i) {
-            first = first->next;
+        ListNode* fast= head;
+        ListNode* slow= head;
+        for(int i=0; i<n; i++){
+            fast=fast->next;
         }
-
-        while (first != nullptr) {
-            first = first->next;
-            second = second->next;
+        if(fast==nullptr){
+            return head->next;
         }
-
-        ListNode* temp = second->next;
-        second->next = second->next->next;
-        delete temp;
-
-        return dummy->next;
+        //fast is pointing at nth node from start and slow at first node
+        //diff btw fast and slow is n;
+        while(fast->next!=nullptr){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        //after moving contantlt together now with same diff, fast is now
+        //at end of list and slow is fast-n i.e. nth node from last
+         ListNode* del = slow->next;
+        slow->next = slow->next->next;
+        
+        // Free the memory of the node to be deleted
+        delete del;
+        return head;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//          ListNode* dummy = new ListNode(0);
+//         dummy->next = head;
+//         ListNode* first = dummy;
+//         ListNode* second = dummy;
+
+//         for (int i = 0; i <= n; ++i) {
+//             first = first->next;
+//         }
+
+//         while (first != nullptr) {
+//             first = first->next;
+//             second = second->next;
+//         }
+
+//         ListNode* temp = second->next;
+//         second->next = second->next->next;
+//         delete temp;
+
+//         return dummy->next;
+//     }
+// };
