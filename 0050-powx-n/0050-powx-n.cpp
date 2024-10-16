706a2c4803a7e0869afd1c6238d84fd585567687
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-10-16 13:31:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long nn = n;
-        if (nn < 0) {
-            x = 1 / x;
-            nn = -nn;
+        if(n==0){
+            return 1;
         }
-        double ans = 1;
-        while (nn) {
-            if (nn % 2 == 1) {
-                ans *= x;
-            }
-            x *= x;
-            nn /= 2;
+        else if(n<0){
+            x=1/x;
+            n=-1*n;
         }
-        return ans;
+        if(n<=1){
+            return x;
+        }
+        if(n%2==0){
+            return myPow(x*x,n/2);
+        }
+        else{
+            return x*myPow(x,n-1);
+        }
     }
 };
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         long long nn = n;
+//         if (nn < 0) {
+//             x = 1 / x;
+//             nn = -nn;
+//         }
+//         double ans = 1;
+//         while (nn) {
+//             if (nn % 2 == 1) {
+//                 ans *= x;
+//             }
+//             x *= x;
+//             nn /= 2;
+//         }
+//         return ans;
+//     }
+// };
