dd05b4c94afb0e6cbc04182a9749b2bb1d7ccb02
Navodita <104451112+Navy1409@users.noreply.github.com>
2024-12-02 09:07:18 +0530

    Time: 0 ms (100%), Space: 8.3 MB (6.42%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -23,3 +23,14 @@ public:
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int isPrefixOfWord(string& sentence, string& searchWord) {
+//         string s=" "+sentence, t=" "+searchWord;
+//         int n=s.size();
+//         int m=s.find(t);
+//         if (m==-1) return -1;
+//         return 1+count(s.begin(), s.begin()+m, ' ');
+//     }
+// };
